5c58f272cf618c28b6bcb19a0f1154ea4da876f1
VincentCastig <vincent.castig@gmail.com>
2018-03-10 16:15:59 -0600

    Album List Get Request

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = (props) => {
-    return (
-        <View>
-            <Text>List</Text>
-        </View>
-    );
-};
+class AlbumList extends Component {
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => {
+            console.log(response.data);
+        });
+    }
+    render() {
+        return (
+            <View>
+                <Text>List</Text>
+            </View>
+        );
+    }
+}
 
 export default AlbumList;
