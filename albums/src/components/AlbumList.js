2777307e834e7378bc78ee03e7e3cc0d2a0926ca
VincentCastig <vincent.castig@gmail.com>
2018-03-10 16:23:18 -0600

    Set Album State

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -3,11 +3,12 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+    state = { albums: [] }
 
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
         .then(response => {
-            console.log(response.data);
+            this.setState({ albums: response.data });
         });
     }
     render() {
