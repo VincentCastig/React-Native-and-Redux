d04c39abe5fa8c94ea76c4c5019177b6d70dee3f
VincentCastig <vincent.castig@gmail.com>
2018-03-10 15:43:53 -0600

    AlbumList Component

--- albums/App.js
+++ albums/App.js
@@ -11,14 +11,8 @@ import {
   Text,
   View
 } from 'react-native';
-import Header from './src/Components/header';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -26,14 +20,7 @@ export default class App extends Component<Props> {
     return (
       <View>
         <Header headerText={'Albums'} />
-        <View style={styles.container}>
-          <Text style={styles.instructions}>
-            To get started, edit App.js
-          </Text>
-          <Text style={styles.instructions}>
-            {instructions}
-          </Text>
-        </View>
+        <AlbumList />
       </View>
     );
   }
