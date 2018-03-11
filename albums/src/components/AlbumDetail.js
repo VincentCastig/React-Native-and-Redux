78d2efe82d8606236201f7e5d36586e28624eae9
VincentCastig <vincent.castig@gmail.com>
2018-03-11 09:46:32 -0500

    CardItem Component

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import Card from './Card';
+import CardItem from './CardItem';
 
 const AlbumList = (props) => {
     return (
         <Card>
-            <Text>{props.album.title}</Text>
+            <CardItem>
+                <View>
+                    {/* <Text>{props.album.title}</Text> */}
+                </View>
+                <View style={styles.headerContentStyle}>
+                    <Text>{props.album.title}</Text>
+                    <Text>{props.album.artist}</Text>
+                </View>
+            </CardItem>
+            {/* <CardItem>
+                <Text>{props.album.title}</Text>
+            </CardItem>
+            <CardItem>
+                <Text>{props.album.title}</Text>
+            </CardItem> */}
         </Card>
     );
 };
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    }
+}
 
 export default AlbumList;
