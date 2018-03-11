049982eb45d052d805650b9bde7c0c63c9688223
VincentCastig <vincent.castig@gmail.com>
2018-03-11 16:21:21 -0500

    TextInput for Login Form

---
+++ auth/App.js
@@ -0,0 +1,31 @@
+
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './src/components/common';
+import LoginForm from './src/components/LoginForm';
+
+type Props = {};
+export default class App extends Component<Props>  {
+  
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBia2zvVaTUf9Qpl9sG3smcGc6dyjhM5Gs",
+      authDomain: "udemyauth-31397.firebaseapp.com",
+      databaseURL: "https://udemyauth-31397.firebaseio.com",
+      projectId: "udemyauth-31397",
+      storageBucket: "udemyauth-31397.appspot.com",
+      messagingSenderId: "915661557885"
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText='header' />
+        <LoginForm />
+      </View>
+    );
+  }
+}
+
--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-// import { Card, CardItem, Button } from './common';
+import { TextInput } from 'react-native';
+import { Card, CardItem, Button } from './common';
 
-class LoginForm extends Component() {
+class LoginForm extends Component {
+    state = { text: '' };
+
     render() {
         return (
-            <View>
-                <Text>d</Text>
-            </View>
-            // <Card>
-            //     <CardItem />
-            //     <CardItem />
-            //     <CardItem>
-            //         <Button>Login</Button>
-            //     </CardItem>
-            // </Card>
+            <Card>
+                <CardItem>
+                    <TextInput 
+                        value={this.state.text}
+                        onChangeText={text => this.setState({ text })}
+                        style={{ height: 20, width: 100 }} />
+                </CardItem>
+                <CardItem />
+                <CardItem>
+                    <Button>Log in</Button>
+                </CardItem>
+            </Card>
         );
     }
 }
+
+
 
 
 export default LoginForm;
