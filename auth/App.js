264994047801531e541fd57a4862f0bc219bab5f
VincentCastig <vincent.castig@gmail.com>
2018-03-11 12:21:19 -0500

    Firebase Initialize App

--- auth/App.js
+++ auth/App.js
@@ -1,15 +1,22 @@
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './src/components/common';
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentWillMount(){
+    firebase.initializeApp ({
+      apiKey: "AIzaSyBia2zvVaTUf9Qpl9sG3smcGc6dyjhM5Gs",
+      authDomain: "udemyauth-31397.firebaseapp.com",
+      databaseURL: "https://udemyauth-31397.firebaseio.com",
+      projectId: "udemyauth-31397",
+      storageBucket: "udemyauth-31397.appspot.com",
+      messagingSenderId: "915661557885"
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
