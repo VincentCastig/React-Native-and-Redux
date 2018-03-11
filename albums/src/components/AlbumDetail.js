793dde964db6203ea36e35ed37562572734ec1d9
VincentCastig <vincent.castig@gmail.com>
2018-03-11 09:57:58 -0500

    Album Image

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardItem from './CardItem';
 
-const AlbumList = (props) => {
+const AlbumList = ({ album }) => {
+    const { title, artist, thumbnail_image } = album;
     return (
         <Card>
             <CardItem>
-                <View>
-                    {/* <Text>{props.album.title}</Text> */}
+                <View style={styles.thumbnailContainerStyle}>
+                    <Image style={styles.thumbnailStyle}source={{ uri: thumbnail_image }} />
                 </View>
                 <View style={styles.headerContentStyle}>
-                    <Text>{props.album.title}</Text>
-                    <Text>{props.album.artist}</Text>
+                    <Text style={styles.headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
                 </View>
             </CardItem>
             {/* <CardItem>
@@ -29,7 +30,20 @@ const styles = {
     headerContentStyle: {
         flexDirection: 'column',
         justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18,
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    thumbnailStyle: {
+        width: 50, 
+        height: 50
     }
-}
+};
 
 export default AlbumList;
