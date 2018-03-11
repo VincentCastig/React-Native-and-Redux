d44c7c4ead25dab544a9d673362bb61c6bcc10a2
VincentCastig <vincent.castig@gmail.com>
2018-03-11 10:08:50 -0500

    Better Scrolling User Experience

--- albums/App.js
+++ albums/App.js
@@ -18,7 +18,7 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
         <Header headerText={'Albums'} />
         <AlbumList />
       </View>
