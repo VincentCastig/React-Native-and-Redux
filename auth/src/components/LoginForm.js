b85c169e7453c864088ccf8dfbc10d42fac736fa
VincentCastig <vincent.castig@gmail.com>
2018-03-12 22:10:08 -0500

    Login Spinner

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Card, CardItem, Button, Input } from './common';
+import { Card, CardItem, Button, Input, Spinner } from './common';
 
 class LoginForm extends Component {
     state = { 
         email: '',
         password: '',
-        error: ''
+        error: '',
+        loading: false
     };
 
     onButtonPress() {
         const { email, password } = this.state;
+        this.setState({ error: '', loading: true });
+
         firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(this.onLoginSucces.bind(this))
         .catch(() => {
             firebase.auth().createUserWithEmailAndPassword(email, password)
-            .catch(() => {
-                this.setState({ error: 'Authentication Failed'})
-            });
+            .then(this.onLoginSucces.bind(this))
+            .catch(this.onLoginFail.bind(this));
         });
     }
+
+    onLoginFail(){
+        this.setState({
+            error: 'Authentication Failed',
+            loading: false
+        });
+     }
+
+    onLoginSucces(){
+       this.setState({
+           email: '',
+           password: '',
+           loading: ''
+       });
+    }
+    renderButton() {
+        if (this.state.loading){
+            return <Spinner />;
+        }
+        return (
+                <Button onPress={this.onButtonPress.bind(this)}>
+                    Log in
+                </Button>
+        )
+    };
 
     render() {
         return (
@@ -41,11 +69,10 @@ class LoginForm extends Component {
                         onChangeText={password => this.setState({ password })}
                     />
                 </CardItem>
+                {this.state.Spinner && <Spinner />}
                 <Text style={styles.errorTextStyle}>{this.state.error}</Text>
                 <CardItem>
-                    <Button onPress={this.onButtonPress.bind(this)}>
-                        Log in
-                    </Button>
+                    {this.renderButton()}
                 </CardItem>
             </Card>
         );
