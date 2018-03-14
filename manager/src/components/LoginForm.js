f273ec72fc4f2b60a58d1f912cc0d73c0bcf3954
VincentCastig <vincent.castig@gmail.com>
2018-03-14 08:14:46 -0500

    Email, Password, and Login Button

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import { Card } from './common';
+import { Card, CardItem, Input, Button } from './common';
 
 class LoginForm extends Component {
     render() {
         return (
-            <Card style={styles.buttonStyle}>
-                <Text>login</Text>
+            <Card >
+                <CardItem>
+                    <Input 
+                        label='Email'
+                        placeholder='email@gmail.com'
+                    />
+                </CardItem>
+
+                <CardItem>
+                    <Input
+                        secureTextEntry 
+                        label='Password'
+                        placeholder='password'
+                    />
+                </CardItem>
+                <CardItem>
+                    <Button>
+                        Login
+                    </Button>
+                </CardItem>
             </Card>
         );
     }
 };
 
-const styles = {
-    buttonStyle: {
-        flex: 1,
-        alignSelf: 'stretch',
-        backgroundColor: '#fdfead',
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: '#007aff',
-        marginLeft: 5,
-        marginRight: 5
-    },
-};
+// const styles = {
+//     buttonStyle: {
+//         flex: 1,
+//         alignSelf: 'stretch',
+//         backgroundColor: '#fdfead',
+//         borderRadius: 5,
+//         borderWidth: 1,
+//         borderColor: '#007aff',
+//         marginLeft: 5,
+//         marginRight: 5
+//     },
+// };
 
 export default LoginForm;
