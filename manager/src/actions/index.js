ba6917e94f8855872dec33d93e006967fcedd81f
VincentCastig <vincent.castig@gmail.com>
2018-03-14 16:06:51 -0500

    Navigation to EmployeeList Component

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import { 
     EMAIL_CHANGED, 
     PASSWORD_CHANGED, 
@@ -40,6 +41,8 @@ const loginUserSuccess = (dispatch, user) => {
         type: LOGIN_USER_SUCCESS, 
         payload: user
     });
+
+    Actions.employeeList();
 };
 
 const loginUserFail = (dispatch) => {
