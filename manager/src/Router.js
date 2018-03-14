3260475bf266004a4b7695d88049c8a46b1864cb
VincentCastig <vincent.castig@gmail.com>
2018-03-14 16:33:35 -0500

    Navigation to EmployeeCreate Component

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
-import EmployeeList from './components/EmployeeList'
+import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
     return (
@@ -10,8 +11,21 @@ const RouterComponent = () => {
                 <Scene key='auth'>
                     <Scene key='login' component={LoginForm} title='Please Login' initial />
                 </Scene>
+
                 <Scene key='main'>
-                    <Scene key='employeeList' component={EmployeeList} title='Employees' />
+                    <Scene 
+                        onRight={() => { Actions.employeeCreate(); }}
+                        rightTitle='Add'
+                        key='employeeList' 
+                        component={EmployeeList} 
+                        title='Employees'
+                        initial 
+                    />
+                    <Scene 
+                        key='employeeCreate' 
+                        component={EmployeeCreate} 
+                        title='Create Employee' 
+                    />
                 </Scene>
             </Scene>
         </Router>
---
+++ manager/src/components/EmployeeCreate.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class EmployeeCreate extends Component {
+    render() {
+        return (
+            <View>
+                <Text>EmployeeCreate</Text>
+                <Text>EmployeeCreate</Text>
+                <Text>EmployeeCreate</Text>
+                <Text>EmployeeCreate</Text>
+            </View>
+        );
+    }
+}
+    
+export default EmployeeCreate;
