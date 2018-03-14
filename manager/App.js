f0f69e2ec4478baa1e7eaa5508dc3677a8eea723
VincentCastig <vincent.castig@gmail.com>
2018-03-14 07:36:43 -0500

    Debugging for Default Reducer

--- manager/App.js
+++ manager/App.js
@@ -6,10 +6,22 @@ import {
 } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './src/reducers';
 
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyAw8tv7Wgcjrdf5PjNcBtMrhgamsWBzpG4",
+      authDomain: "manager-a7229.firebaseapp.com",
+      databaseURL: "https://manager-a7229.firebaseio.com",
+      projectId: "manager-a7229",
+      storageBucket: "manager-a7229.appspot.com",
+      messagingSenderId: "1048620897068"
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <Provider store={createStore(reducers)}>
