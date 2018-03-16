87f45718f2b017c59a4f7d7073b26df5333daf93
VincentCastig <vincent.castig@gmail.com>
2018-03-15 21:03:06 -0500

    Default Value of Monday for Shift Day

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardItem, Input, Button } from './common';
 
 
 class EmployeeCreate extends Component {
+    
+    onButtonPress() {
+        const { name, phone, shift } = this.props;
+
+        this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+    }
+
     render() {
         return (
             <Card>
@@ -44,7 +51,7 @@ class EmployeeCreate extends Component {
                 </CardItem>
 
                 <CardItem>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Create
                     </Button>
                 </CardItem>
@@ -67,4 +74,7 @@ const styles = {
         return { name, phone, shift };
     };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, { 
+    employeeUpdate, 
+    employeeCreate 
+})(EmployeeCreate);
